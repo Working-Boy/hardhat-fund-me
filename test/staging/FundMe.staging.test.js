456ff988259912b66beb647b2cd0c6716586b97e
ADEYANJU QUASIM <workingboyquasim@gmail.com>
2025-05-07 12:19:14 +0100

    updated test folder

--- test/staging/FundMe.staging.test.js
+++ test/staging/FundMe.staging.test.js
@@ -1,21 +1,31 @@
-const { getNamedAccounts, ethers } = require("hardhat");
-const { developmentChains } = require("../../helper-hardhat-config");
-const { assert } = require("chai");
+const { assert } = require("chai")
+const { network, ethers, getNamedAccounts } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
 
 developmentChains.includes(network.name)
-  ? describe.skip
-  : describe("FundMe Staging Tests", function () {
-      let fundMe, deployer;
-      const sendValue = ethers.utils.parseEther("1");
-      beforeEach(async function () {
-        deployer = (await getNamedAccounts()).deployer;
-        fundMe = await ethers.getContract("FundMe", deployer);
-      });
+    ? describe.skip
+    : describe("FundMe Staging Tests", function () {
+          let deployer
+          let fundMe
+          const sendValue = ethers.utils.parseEther("0.1")
+          beforeEach(async () => {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
 
-      it("allows people to fund and withdraw", async function () {
-        await fundMe.fund({ value: sendValue });
-        await fundMe.withdraw();
-        const endingBalance = await fundMe.provider.getBalance(fundMe.address);
-        assert.equal(endingBalance.toString(), "0");
-      });
-    }); // RUN THIS ONLY IF WE ARE NOT IN A DEVELOPMENT CHAIN.
+          it("allows people to fund and withdraw", async function () {
+              const fundTxResponse = await fundMe.fund({ value: sendValue })
+              await fundTxResponse.wait(1)
+              const withdrawTxResponse = await fundMe.withdraw()
+              await withdrawTxResponse.wait(1)
+
+              const endingFundMeBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              console.log(
+                  endingFundMeBalance.toString() +
+                      " should equal 0, running assert equal..."
+              )
+              assert.equal(endingFundMeBalance.toString(), "0")
+          })
+      })
